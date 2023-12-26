ed20fbf67b7a1fbf5e34ed079d488d7eacd9c364
SURAJ <77872306+DrSuraj@users.noreply.github.com>
2023-12-26 16:52:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-function shortcut(s1, s2) {
-  // your code here
+function shortcut(str1: string, str2: string): string {
+    // Check if either input string is empty
+    if (str1.length === 0 || str2.length === 0) {
+        return '';
+    }
+
+    // Return the initial letters of the input strings
+    return str1.charAt(0).toUpperCase() + str2.charAt(0).toUpperCase();
 }
 
-// Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
+// Examples
+console.log(shortcut("Amnesty", "International"));  // Output: 'AI'
+console.log(shortcut("Hello", "world"));            // Output: 'Hw'
+console.log(shortcut("", "International"));          // Output: ''
+console.log(shortcut("Amnesty", ""));                // Output: ''
